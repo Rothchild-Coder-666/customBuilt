3cefab82d2634575dd5fbbd1b67f78e9c845241e
Rothchild-Coder-666 <nastynate.rothchild@gmail.com>
2025-02-24 20:27:17 -0500

    Create ICurveMetaRegistry.sol

---
+++ ICurveMetaRegistry.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.20;
+
+interface ICurveMetaRegistry {
+    function get_pool_params(address _pool, uint256 _handler_id) external view returns (uint256[] memory);
+    function get_base_pool(address _pool, uint256 _handler_id) external view returns (address);
+    function is_meta(address _pool, uint256 _handler_id) external view returns (bool);
+    function get_coin_addresses(address _pool, uint256 _handler_id) external view returns (address[] memory);
+    function get_underlying_coins(address _pool, uint256 _handler_id) external view returns (address[] memory);
+    function get_decimals(address _pool, uint256 _handler_id) external view returns (uint256[] memory);
+    function get_underlying_decimals(address _pool, uint256 _handler_id) external view returns (uint256[] memory);
+    function get_lp_token(address _pool, uint256 _handler_id) external view returns (address);
+    function get_A(address _pool, uint256 _handler_id) external view returns (uint256);
+    function get_fee(address _pool, uint256 _handler_id) external view returns (uint256);
+    function get_virtual_price(address _pool, uint256 _handler_id) external view returns (uint256);
+    function get_dy(address _pool, uint256 _handler_id, int128 i, int128 j, uint256 dx) external view returns (uint256);
+    function get_dy_underlying(address _pool, uint256 _handler_id, int128 i, int128 j, uint256 dx) external view returns (uint256);
+    function get_n_coins(address _pool, uint256 _handler_id) external view returns (uint256);
+    function get_n_underlying_coins(address _pool, uint256 _handler_id) external view returns (uint256);
+    function get_pool_from_lp_token(address _lp_token, uint256 _handler_id) external view returns (address);
+    function get_coin_indices(address _pool, address[2] calldata _coins, uint256 _handler_id) external view returns (int128[2] memory);
+    // Add more functions as needed
+}
