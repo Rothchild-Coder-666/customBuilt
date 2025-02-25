01df4a2f4b3c1849397c28a512d63ff32f661f0a
Rothchild-Coder-666 <nastynate.rothchild@gmail.com>
2025-02-25 17:54:34 -0500

    Update ICurveMetaRegistry.sol

--- ICurveMetaRegistry.sol
+++ ICurveMetaRegistry.sol
@@ -1,29 +1,23 @@
-pragma solidity ^0.8.20;
+//SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.6.0 <0.9.0;
 
-interface ICurveMetaRegistry {
-   // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+interface ICurveRegistry {
+    function find_pool_for_coins(address _from, address _to, uint256 i) external view returns (address);
+       
+    function get_n_coins(address pool) external view returns (uint256, uint256);
 
-interface ICurveMetaRegistry {
-    function find_pool_for_coins(address _from, address _to, uint256 i)
-        external
-        view
-        returns (address);
-    function get_coin_indices(address _pool, address _from, address _to) external view returns (int128, int128, bool);
-    function get_base_pool(address _pool, uint256 _handler_id) external view returns (address);
-    function is_meta(address _pool, uint256 _handler_id) external view returns (bool);
-    function get_coin_addresses(address _pool, uint256 _handler_id) external view returns (address[] memory);
-    function get_underlying_coins(address _pool, uint256 _handler_id) external view returns (address[] memory);
-    function get_decimals(address _pool, uint256 _handler_id) external view returns (uint256[] memory);
-    function get_underlying_decimals(address _pool, uint256 _handler_id) external view returns (uint256[] memory);
-    function get_lp_token(address _pool, uint256 _handler_id) external view returns (address);
-    function get_A(address _pool, uint256 _handler_id) external view returns (uint256);
-    function get_fee(address _pool, uint256 _handler_id) external view returns (uint256);
-    function get_virtual_price(address _pool, uint256 _handler_id) external view returns (uint256);
-    function get_dy(address _pool, uint256 _handler_id, int128 i, int128 j, uint256 dx) external view returns (uint256);
-    function get_dy_underlying(address _pool, uint256 _handler_id, int128 i, int128 j, uint256 dx) external view returns (uint256);
-    function get_n_coins(address _pool, uint256 _handler_id) external view returns (uint256);
-    function get_n_underlying_coins(address _pool, uint256 _handler_id) external view returns (uint256);
+    function get_coins(address pool) external view returns (address[8] memory);
+
+    function get_coin_indices(address pool, address _from, address _to) external view returns (int128, int128, bool);
+
+    function get_pool_asset_type(address pool) external view returns (uint256);
+
+    function get_lp_token(address pool) external view returns (address);
+
+    function get_pool_from_lp_token(address lp) external view returns (address);
+
+    function get_virtual_price_from_lp_token(address lp) external view returns (uint256);
+}
     function get_pool_from_lp_token(address _lp_token, uint256 _handler_id) external view returns (address);
 }
 
