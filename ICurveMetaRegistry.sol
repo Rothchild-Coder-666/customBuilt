1dac8579e3679b26e76b3f0230706c3c1cd73e97
Rothchild-Coder-666 <nastynate.rothchild@gmail.com>
2025-02-24 23:46:06 -0500

    Update ICurveMetaRegistry.sol

--- ICurveMetaRegistry.sol
+++ ICurveMetaRegistry.sol
@@ -1,7 +1,8 @@
 pragma solidity ^0.8.20;
 
 interface ICurveMetaRegistry {
-    function get_pool_params(address _pool, uint256 _handler_id) external view returns (uint256[] memory);
+    function find_pool_for_coins(address _from, address _to) external view returns (address);
+    function get_coin_indices(address _pool, address _from, address _to) external view returns (int128, int128, bool);function get_pool_params(address _pool, uint256 _handler_id) external view returns (uint256[] memory);
     function get_base_pool(address _pool, uint256 _handler_id) external view returns (address);
     function is_meta(address _pool, uint256 _handler_id) external view returns (bool);
     function get_coin_addresses(address _pool, uint256 _handler_id) external view returns (address[] memory);
@@ -17,7 +18,5 @@ interface ICurveMetaRegistry {
     function get_n_coins(address _pool, uint256 _handler_id) external view returns (uint256);
     function get_n_underlying_coins(address _pool, uint256 _handler_id) external view returns (uint256);
     function get_pool_from_lp_token(address _lp_token, uint256 _handler_id) external view returns (address);
-    function find_pool_for_coins(address _from, address _to) external view returns (address);
-    function get_coin_indices(address _pool, address _from, address _to) external view returns (int128, int128, bool);
 }
 
