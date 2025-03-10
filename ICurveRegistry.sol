a54496d1d292a1fc8bf1e07d32cbed15d1918951
Rothchild-Coder-666 <nastynate.rothchild@gmail.com>
2025-03-10 19:11:33 -0400

    ICurveRegistry.sol

--- ICurveRegistry.sol
+++ ICurveRegistry.sol
@@ -6,11 +6,13 @@ interface ICurveRegistry {
 
   function get_coins(address pool) external view returns (address[8] memory);
 
+  function get_underlying_coins(address pool) external view returns (address[8]);
+
   function get_pool_from_lp_token(address lp) external view returns (address);
 
   function pool_list(uint256 id) external view returns (address);
 
-  function find_pool_for_coins(address _from, address _to)
+  function find_pool_for_coins(address _from, address _to, uint256 i)
         external
         view
         returns (address);
