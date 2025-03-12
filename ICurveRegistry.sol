a854358e3e15e09693d4e164f978ffbdd2fec65d
Rothchild-Coder-666 <nastynate.rothchild@gmail.com>
2025-03-11 21:59:43 -0400

    ICurveRegistry.sol

--- ICurveRegistry.sol
+++ ICurveRegistry.sol
@@ -2,22 +2,22 @@
 pragma solidity >=0.8.0;
 
 interface ICurveRegistry {
-  function get_n_coins(address lp) external view returns (uint256);
+    function get_n_coins(address lp) external view returns (uint256);
 
-  function get_coins(address pool) external view returns (address[8] memory);
+    function get_coins(address pool) external view returns (address[8] memory);
 
-  function get_underlying_coins(address pool) external view returns (address[8]);
+    function get_underlying_coins(address pool) external view returns (address[8] memory);
 
-  function get_pool_from_lp_token(address lp) external view returns (address);
+    function get_pool_from_lp_token(address lp) external view returns (address);
 
-  function pool_list(uint256 id) external view returns (address);
+    function pool_list(uint256 id) external view returns (address);
 
-  function find_pool_for_coins(address _from, address _to, uint256 i)
+    function find_pool_for_coins(address _from, address _to, uint256 i)
         external
         view
         returns (address);
   
-  function get_virtual_price_from_lp_token(
+    function get_virtual_price_from_lp_token(
         address lp
     ) external view returns (uint256);
 
